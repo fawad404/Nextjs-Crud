455ac5d8836f8557830f82a96b1937ee483535de
fawad404 <159186424+fawad404@users.noreply.github.com>
2024-06-13 09:11:10 -0700

    TopicsList.jsx

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,49 +1,37 @@
 import Link from "next/link";
-import RemoveBtn from "./RemoveBtn";
+import RemoveBtn from "@/components/RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
+import { getTopics } from "@/lib/api";
 
-const getTopics = async () => {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-    try {
-        const res = await fetch(`${apiUrl}/api/topics`, {
-            cache: "no-store",
-        });
+export async function getServerSideProps() {
+  const topics = await getTopics();
+  return {
+    props: { topics },
+  };
+}
 
-        if (!res.ok) {
-            throw new Error("Failed to fetch topics");
-        }
+export default function TopicsList({ topics }) {
+  if (!topics.length) {
+    return <div>No topics available.</div>;
+  }
 
-        return res.json();
-    } catch (err) {
-        console.log("Error loading topics: ", err);
-        
-    }
-};
+  return (
+    <>
+      {topics.map((t) => (
+        <div key={t._id} className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
+          <div>
+            <h2 className="font-bold text-2xl">{t.title}</h2>
+            <div>{t.description}</div>
+          </div>
 
-export default async function TopicsList() {
-    const { topics } = await getTopics();
-
-    if (!topics.length) {
-        return <div>No topics available.</div>;
-    }
-
-    return (
-        <>
-            {topics.map(t => (
-                <div key={t._id} className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
-                    <div>
-                        <h2 className="font-bold text-2xl">{t.title}</h2>
-                        <div>{t.description}</div>
-                    </div>
-
-                    <div className="flex gap-2">
-                        <RemoveBtn id={t._id}/>
-                        <Link href={`/editTopic/${t._id}`}>
-                            <HiPencilAlt size={24} />
-                        </Link>
-                    </div>
-                </div>
-            ))}
-        </>
-    );
+          <div className="flex gap-2">
+            <RemoveBtn id={t._id} />
+            <Link href={`/editTopic/${t._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
